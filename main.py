03355d70a3547e08667db8308422d01565074d55
Baatyrbek <batrudinovshapiro@gmail.com>
2025-06-15 21:19:56 -0500

    added readme + modular format

--- main.py
+++ main.py
@@ -1,63 +1,27 @@
 import keyboard
+import user_manager
 
-userList = {}
-print("\nPlease press a corresponding key:\n" \
-"S to Show list\n" \
-"C to Create new user\n" \
-"R to Remove a user\n" \
-"Q to Quit")
+def main():
+    print("\nPlease press a corresponding key:\n" \
+            "S to Show list\n" \
+            "C to Create new user\n" \
+            "R to Remove a user\n" \
+            "Q to Quit\n")
 
-while True:
+    while True:
+        key = keyboard.read_key().lower()
 
-    key = keyboard.read_key().lower()
-
-    if key == "s":
-        try:
-            rootPassword = int(input("Admin password:\n"))
-        except ValueError:
-            print("Invalid password! Must be a number.\n")
-            continue
-        if rootPassword == 1234:
-            print(f"User List:\n{userList}")
+        if key == "s":
+            user_manager.show_users()
+        elif key == "c":
+            user_manager.create_user()
+        elif key == "r":
+            user_manager.remove_user()
+        elif key == "q":
+            print("Exiting... Goodbye!\n")
+            break
         else:
-            print("Incorrect admin password.\n")
+            print("Invalid key! Please use S, C, R, or Q.\n")
 
-    elif key == "c":
-        login = input("Login:\n").strip()
-        if login in userList:
-            print("This user already exists.\n")
-            continue
-        try:
-            password = int(input("Password:\n"))
-        except ValueError:
-            print("Invalid input! Password must be a number.\n")
-            continue
-        userList[login] = password
-        print(f"New user '{login}' was added.\n")
-
-    elif key == "r":
-        login = input("Enter your login:\n").strip()
-        if login in userList:
-            try:
-                password = int(input("Password:\n"))
-            except ValueError:
-                print("Invalid input! Password must be a number.\n")
-                continue
-            if userList.get(login) == password:
-                userAgree = input("Are you sure you want to delete your account?\nYes/No? ").lower().strip()
-                if userAgree == "yes":
-                    del userList[login]
-                    print(f"User '{login}' was removed.\n")
-                else:
-                    print("Account was not deleted.\n")
-            else:
-                print("Oops, wrong password.\n")
-        else:
-            print("Sorry, that user does not exist.\n")
-
-    elif key == "q":
-        print("Exiting... Goodbye!\n")
-        break
-
-    else:
-        print("Invalid key! Please use S, C, R, or Q.\n")
+if __name__ == "__main__":
+    main()
